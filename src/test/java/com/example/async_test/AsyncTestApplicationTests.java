d26fc32d2324e183e6771a0736290c2c51dafe16
junyeong <junyeong@infobank.net>
2022-04-13 15:31:52 +0900

    Initial commit

---
+++ src/test/java/com/example/async_test/AsyncTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.async_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
