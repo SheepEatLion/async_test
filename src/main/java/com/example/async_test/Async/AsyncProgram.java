0edf98e5fe3ac179cbc071b232907d676894aec7
junyeong <junyeong@infobank.net>
2022-04-14 14:51:45 +0900

    feat: add Async code log
    - 실행 결과 이미지 추가

--- src/main/java/com/example/async_test/Async/AsyncProgram.java
+++ src/main/java/com/example/async_test/Async/AsyncProgram.java
@@ -10,21 +10,26 @@ import java.util.stream.IntStream;
 @Log4j2
 public class AsyncProgram {
     private  static final int THREAD_POOL_SIZE = 100;
-    private static final Executor executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+    private static final ExecutorService executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
 
     public static void main(String[] args) {
         IntStream.rangeClosed(1, 100).forEach(n -> {
                     String threadName = Thread.currentThread().getName();
-                    log.info("sync : " + threadName + " : start " + n + " th");
-                    executor.execute(AsyncProgram::asyncTask);  // 변경점
-                    log.info("sync : " + threadName + " : end " + n + " th");
+                    //log.info("sync : " + threadName + " : start " + n + " th");
+                    executor.submit(() -> asyncTask(n));
+                    //executor.execute(AsyncProgram::asyncTask(n));  // 변경점
+                    //log.info("sync : " + threadName + " : end " + n + " th");
                 }
         );
+
     }
 
-    private static void asyncTask(){
+    private static void asyncTask(int n){
         try{
+            String threadName = Thread.currentThread().getName();
+            log.info("sync : " + threadName + " : start " + n + " th");
             Thread.sleep(1000);
+            log.info("sync : " + threadName + " : end " + n + " th");
         } catch (InterruptedException e){
             log.error(e.getMessage());
         }
