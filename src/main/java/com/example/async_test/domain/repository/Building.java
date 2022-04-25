d00c412223459a929c5b263a2449523c1dc0c82e
SheepEatLion <gowjr207@gmail.com>
2022-04-25 18:06:27 +0900

    init jpa repository

---
+++ src/main/java/com/example/async_test/domain/repository/Building.java
@@ -0,0 +1,6 @@
+package com.example.async_test.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Building extends JpaRepository<Building, Long> {
+}
--- src/main/java/com/example/async_test/domain/repository/ElevatorRepository.java
+++ src/main/java/com/example/async_test/domain/repository/ElevatorRepository.java
@@ -1,4 +1,7 @@
 package com.example.async_test.domain.repository;
 
-public class ElevatorRepository {
+import com.example.async_test.domain.Elevator;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ElevatorRepository extends JpaRepository<Elevator, Long> {
 }
