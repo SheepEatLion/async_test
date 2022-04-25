0abc2f06c14e60c85e0e0bab67138adf54f11a30
SheepEatLion <gowjr207@gmail.com>
2022-04-25 20:37:15 +0900

    fix: 파라미터 수정

--- src/main/java/com/example/async_test/domain/repository/Building.java
+++
@@ -1,6 +0,0 @@
-package com.example.async_test.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Building extends JpaRepository<Building, Long> {
-}
