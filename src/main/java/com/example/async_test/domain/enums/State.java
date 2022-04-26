b537b6d1784a5bfb984ad287908a21165b4f6a3c
SheepEatLion <gowjr207@gmail.com>
2022-04-26 13:15:24 +0900

    update: enum class
    - 구조 변경
    - 상태 카테고리 추가(UP_OPEN)

---
+++ src/main/java/com/example/async_test/domain/enums/State.java
@@ -0,0 +1,17 @@
+package com.example.async_test.domain.enums;
+
+/**
+ * OPEN: 문 열림
+ * CLOSE: 문 닫힘
+ * ERROR: 오류
+ */
+
+public enum State {
+    OPEN,
+    CLOSE,
+    UP,
+    DOWN,
+    UP_OPEN,
+    DOWN_OPEN,
+    ERROR
+}
